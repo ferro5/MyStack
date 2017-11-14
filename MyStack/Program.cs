2a2c7e4a2d9e7e806261e382b7cbb2f977a50ba9
ferro5 <ferro5@inbox.ru>
2017-11-14 20:27:28 +0200

    Добавьте файлы проекта.

---
+++ MyStack/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stack = new MyStack();
+            stack.Push(2);
+            stack.Push(6);
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(1);
+            stack.Push(3);
+
+            for (int i = 0; i < stack.Conut; i++)
+            {
+                Console.WriteLine(stack.ToString());
+                
+            }
+            
+            
+
+            var head = stack.Pop();
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+            for (int i = 0; i < stack.Conut; i++)
+            {
+                Console.WriteLine(stack.ToString());
+            }
+            Console.WriteLine(head);
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+            head = stack.Peek();
+            Console.WriteLine(head);
+            Console.ReadKey();
+        }
+    }
+
+     public class MyStack
+     {
+         private int[] _items;
+         private int _count;
+         private int n = 10;
+
+         public MyStack()
+         {
+             _items = new int[n];
+         }
+
+         public MyStack(int lenght)
+         {
+             _items = new int[lenght];
+         }
+
+         public bool isEmpty()
+         {
+             return _count == 0;
+         }
+
+         public int Conut
+         {
+             get { return _count; }
+         }
+
+         public void Push(int item)
+         {
+             if (_count==_items.Length)
+             {
+                 Console.WriteLine("Stack is overflow");
+             }
+             _items[_count++] = item;
+         }
+
+         public int Pop()
+         {
+             if (isEmpty())
+             {
+                 Console.WriteLine($"Stack is empty");
+             }
+             int item = _items[--_count];
+            _items[_count] = default(int);
+             return item;
+         }
+
+         public int Peek()
+         {
+             return _items[_count-1];
+         }
+
+         public override string ToString()
+         {
+             return string.Format($"");
+         }
+     }
+}
---
+++ MyStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MyStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyStack")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("48515224-84f5-4c97-91b6-3c6992a7a013")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
